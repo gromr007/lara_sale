eb90b42263bcbc748bd6d6256355584de23b7e30
grom_ubuntu <gromr007@yandex.ru>
2024-02-25 17:48:18 +0300

    Фикс серверного бага фабрики, + запускающий скрипт композера

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,12 +8,6 @@ use Illuminate\Support\Str;
 
 class ProductFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected string $model = Product::class;
 
     /**
      * Define the model's default state.
